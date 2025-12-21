24dcb8dbaddbe4c1b0b9fe54b9851bab86c2ae28
Muhammad Usman Nazir <nazirusman721@gmail.com>
2025-12-21 12:52:36 +0500

    Add VerifyEmail route and remove Temp_Verify

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import VerifyEmail from './components/VerifyEmail';
 import Dashboard from './components/Dashboard';
 import OwnerDashboard from './components/OwnerDashboard';
 import './App.css';
-import Verify from './components/Temp_Verify';
-
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/verify-email/:token" element={<VerifyEmail />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/owner-dashboard" element={<OwnerDashboard />} />
-        <Route path="/verify/:token" element={<Verify />} />
       </Routes>
     </Router>
   );
