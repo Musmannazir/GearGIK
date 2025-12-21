51c3568dccef35cdc6d56721f561930010bd7803
Muhammad Usman Nazir <nazirusman721@gmail.com>
2025-12-21 15:10:13 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
-import VerifyEmail from './components/VerifyEmail';
 import Dashboard from './components/Dashboard';
 import OwnerDashboard from './components/OwnerDashboard';
 import './App.css';
@@ -12,7 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/verify-email/:token" element={<VerifyEmail />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/owner-dashboard" element={<OwnerDashboard />} />
       </Routes>
